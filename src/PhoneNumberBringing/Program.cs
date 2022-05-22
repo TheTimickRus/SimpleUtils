73b2073119a9c8f80fb408bd1783dcd6c733057f
TheTimickRus <andrey.timofeev.and37@gmail.com>
2022-05-22 08:03:34 +0300

    Init #1

---
+++ src/PhoneNumberBringing/Program.cs
@@ -0,0 +1,31 @@
+﻿// Приведение номеров к виду +7**********, удаление повторов
+
+using System.Text.RegularExpressions;
+
+var inpData = File.ReadAllLines("Inp.txt")
+    .Select(s => new Regex("\\d+").Match(s).Value)
+    .Select(s => new Regex("^3").Replace(s, "+73"))
+    .Select(s => new Regex("^4").Replace(s, "+74"))
+    .Select(s => new Regex("^7").Replace(s, "+7"))
+    .Select(s => new Regex("^8").Replace(s, "+7"))
+    .Select(s => new Regex("^9").Replace(s, "+79"))
+    .Where(s => s.Length > 7)
+    .Distinct();
+
+/*
+for (var i = 0; i < arr.Count; i++)
+{
+    switch (arr[i].Length)
+    {
+        case 10:
+            arr[i] = arr[i].Insert(0, "+7");
+            break;
+        case 11 when arr[i][0] == '7' || arr[i][0] == '8':
+            arr[i] = arr[i].Remove(0, 1);
+            arr[i] = arr[i].Insert(0, "+7");
+            break;
+    }
+}
+*/
+
+File.WriteAllLines("Out.txt", inpData);
---
+++ src/PhoneNumberInformation/Program.cs
@@ -0,0 +1,45 @@
+﻿// Получение данных о номере с сервисов zvonili.com и numbers.ba-za.net
+
+using HtmlAgilityPack;
+
+var numbers = File.ReadAllLines("Inp.txt");
+var results = new List<string>();
+
+var web = new HtmlWeb();
+foreach (var number in numbers)
+{
+    try
+    {
+        var response = web.Load($"https://zvonili.com/phone/{number}");
+        var node = response.DocumentNode.SelectSingleNode("/html/body/div[2]/div[1]/div[1]/div[4]/span");
+
+        if (node != null && node.InnerHtml != "")
+        {
+            results.Add($"{number}, {node.InnerHtml}");
+            Console.WriteLine($"{number}, {node.InnerHtml}");
+            continue;
+        }
+        
+        response = web.Load($"https://numbers.ba-za.net/number/russia/{number}/");
+        node = response.DocumentNode.SelectSingleNode("//*[@id=\"info_number\"]/div[3]/strong");
+        
+        if (node != null && node.InnerHtml != "▓▓▓▓▓▓▓▓▓▓▓▓")
+        {
+            results.Add($"{number}, {node.InnerHtml}");
+            Console.WriteLine($"{number}, {node.InnerHtml}");
+            continue;
+        }
+        
+        results.Add($"{number}, НЕТ ДАННЫХ!");
+        Console.WriteLine($"{number}, НЕТ ДАННЫХ!");
+    }
+    catch (Exception ex)
+    {
+        results.Add($"{number}, {ex.Message}");
+        Console.WriteLine($"{number}, {ex.Message}");
+    }
+}
+
+File.WriteAllLines("Out.txt", results);
+
+return 0;
